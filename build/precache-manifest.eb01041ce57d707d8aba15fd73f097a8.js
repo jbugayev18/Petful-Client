7e27384b86b0cdba35396c5c36d968e742196bd0
Julia Bugayev <jbugayev18@gmail.com>
2020-10-21 13:46:43 -0700

    Updated client

---
+++ build/precache-manifest.eb01041ce57d707d8aba15fd73f097a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987c527ccc59b04b426b60f62e444433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981ec4c7ece78cd42c26",
+    "url": "/static/css/main.5375ee5a.chunk.css"
+  },
+  {
+    "revision": "ec4858fb1bf5b530d963",
+    "url": "/static/js/2.414acc3c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.414acc3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981ec4c7ece78cd42c26",
+    "url": "/static/js/main.4f6890d4.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "38d7af7060f290241421659a2420724b",
+    "url": "/static/media/paw-1s-1084px.38d7af70.svg"
+  }
+]);
